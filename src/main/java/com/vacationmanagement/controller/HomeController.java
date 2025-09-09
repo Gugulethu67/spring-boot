711a974094df7a53b94a60797c2fcb49e2d7583c
Gugulethu Mbele <gugulethu@capetown.travel>
2025-09-09 08:56:32 +0200

    Add Home Controller

---
+++ src/main/java/com/vacationmanagement/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.vacationmanagement.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Vacation Management API is running 🚀";
+    }
+}
+
